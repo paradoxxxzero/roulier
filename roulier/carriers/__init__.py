1b9d2e737bb385e713aa7989449180b1c093daf5
Florian Mounier <paradoxxx.zero@gmail.com>
2021-11-03 12:55:06 +0100

    Implement MondialRelay get_label, get_label_url, find_pickup_site

--- roulier/carriers/__init__.py
+++ roulier/carriers/__init__.py
@@ -3,5 +3,6 @@ from .gls_fr import rest as gls_fr_rest
 from .gls_fr import glsbox as gls_fr_glsbox
 from . import chronopost_fr
 from . import dpd_fr_soap
+from . import mondial_relay
 
 # from . import geodis
